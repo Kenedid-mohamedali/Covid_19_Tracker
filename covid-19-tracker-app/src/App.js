1199e0adf03cc482ecc3596096033cefdbec2661
Kénédid <kenedid.mohamedali@gmail.com>
2021-05-18 02:21:22 +0200

    2nd commit

--- covid-19-tracker-app/src/App.js
+++ covid-19-tracker-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { MenuItem, FormControl, Select } from '@material-ui/core';
+import { MenuItem, FormControl, Select, Card, CardContent } from '@material-ui/core';
 import InfoBox from './InfoBox';
 import Map from './Map';
 import './App.css';
@@ -7,37 +7,58 @@ import './App.css';
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
 
-  useEffect(() => {
-    const getCountriesData = async () => {
-      await fetch("https://disease.sh/v3/covid-19/countries")
-        .then((response) => response.json())
-        .then((data) => {
-          const countries = data.map((country) => ({
-            name: country.country, // United States, France, ...
-            value: country.countryInfo.iso2, // USA, FR
-          }));
+    useEffect(() => {
+      const getCountriesData = async () => {
+        await fetch("https://disease.sh/v3/covid-19/countries")
+          .then((response) => response.json())
+          .then((data) => {
+            const countries = data.map((country) => ({
+              name: country.country, // United States, France, ...
+              value: country.countryInfo.iso2, // USA, FR
+            }));
 
-          setCountries(countries);
-        });
-    };
+            setCountries(countries);
+          });
+      };
 
       getCountriesData();
     }, []);
 
-    const onCountryChange = (event) => {
+    const onCountryChange = async (event) => {
       const countryCode = event.target.value;
-      console.log("WORKS OR NOT?", countryCode);
       setCountry(countryCode);
+
+      const url =
+        countryCode === "worldwide"
+          ? "https://disease.sh/v3/covid-19/all"
+          : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+      
+      await fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          setCountry(countryCode);
+
+          // All the data...
+          // from the country response
+          setCountryInfo(data);
+        })
     };
 
+  console.log("COUNTRY INFO >>>", countryInfo);
+  
     return (
       <div className="app">
         <div className="app__left">
           <div className="app__header"> 
             <h1>COVID-19 TRACKER</h1>
             <FormControl className="app__dropdown">
-              <Select variant="outlined" onChange={onCountryChange} value={country}>
+              <Select
+                variant="outlined"
+                onChange={onCountryChange}
+                value={country}
+              >
                 <MenuItem value="worldwide">Worldwide</MenuItem>
                 {countries.map((country) => (
                   <MenuItem value={country.value}>{country.name}</MenuItem>
@@ -51,19 +72,17 @@ function App() {
             <InfoBox title="Recovered" cases={1234} total={3000} />
             <InfoBox title="Deaths" cases={12345} total={4000} />
           </div>
-
-          <Map />
-                          {/* Header DONE */}
-                          {/* Title + Select input dropdown field DONE */}
-                          {/* Infoboxs title="Coronavirus cases" DONE*/}
-                          {/* Infoboxs title="Coronavirus recoveries" DONE */}
-                          {/* Infoboxs Deaths DONE */}          
-                          {/* Table */}
-                          {/* Graph */}
-                          {/* Map */}
-        </div>
-                        
-        <div className="app_right"></div>
+                          
+          <Map />                                                                                        
+        </div>                        
+        <Card className="app__right">
+          <CardContent>
+            <h3>Live Cases by Country</h3>
+            {/* Table */}
+            <h3>Worldwide new cases</h3>
+            {/* Graph */}                          
+          </CardContent>                                    
+        </Card>
       </div>
     );
 }
